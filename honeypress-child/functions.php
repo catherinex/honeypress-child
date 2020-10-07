e17e1583e87c11849f89fda6bdc3bc0ac739ea8e
xiang <x.xu@reply.de>
2020-10-08 01:16:48 +0200

    added cancel button on contact form

--- honeypress-child/functions.php
+++ honeypress-child/functions.php
@@ -16,7 +16,7 @@ function my_plugin_add_stylesheet() {
     wp_enqueue_style( 'mp-style-homepage', get_stylesheet_directory_uri() . '/css/homepage.css', array(), rand(111,9999));
     wp_enqueue_style( 'mp-style-footer', get_stylesheet_directory_uri() . '/css/footer.css');
     wp_enqueue_style( 'mp-style-button', get_stylesheet_directory_uri() . '/css/button.css', array(), rand(111,9999));
-    wp_enqueue_style( 'mp-style-contactus', get_stylesheet_directory_uri() . '/css/contactus.css');
+    wp_enqueue_style( 'mp-style-contactus', get_stylesheet_directory_uri() . '/css/contactus.css', array(), rand(111,9999));
     wp_enqueue_style( 'mp-style-menu', get_stylesheet_directory_uri() . '/css/menu.css');
     wp_enqueue_style( 'mp-style-insightpost', get_stylesheet_directory_uri() . '/css/insightpost.css');
     wp_enqueue_style( 'mp-style-insights', get_stylesheet_directory_uri() . '/css/insights.css');
