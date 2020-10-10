dc90a1d5c1e0fb37f0f002ddce024d7b5c03c872
xiang <x.xu@reply.de>
2020-10-10 23:33:45 +0200

    adjusted footer for tablet and mobile view

--- honeypress-child/sidebar-footer.php
+++ honeypress-child/sidebar-footer.php
@@ -16,12 +16,12 @@
 		}
 		for($i=2; $i<=2; $i++)
 		{
-			echo '<div class="col-lg-4 col-md-6 col-sm-12">';
+			echo '<div class="col-lg-4 col-md-6 col-sm-6 col-6">';
 			dynamic_sidebar('footer-sidebar-'.$i);
 			echo '</div>';
 		}
 		?>
-		<div class="col-lg-4 col-md-6 col-sm-12">
+		<div class="col-lg-4 col-md-6 col-sm-6 col-6">
 
 		      <aside id="nav_menu-3" class="widget widget_text sml-device widget_news">
 		        <h3 class="widget-title">News</h3>
@@ -44,17 +44,45 @@
 					<?php } ?>
 						  </ul>
 					  </div>
-					</aside>
+					</aside></div>
 		    <?php } else {
-					for($i=1; $i<=3; $i++)
+					for($i=1; $i<=1; $i++)
 					{
-						echo '<div class="col-lg-4 col-md-6 col-sm-12">';
+						echo '<div class="col-lg-4 col-md-12 col-sm-12">';
 						dynamic_sidebar('footer-sidebar-'.$i);
 						echo '</div>';
 					}
-				}?>
-
-		</div>
+					for($i=2; $i<=3; $i++)
+					{
+						echo '<div class="col-lg-4 col-md-6 col-sm-6 col-6">';
+						dynamic_sidebar('footer-sidebar-'.$i);
+						echo '</div>';
+					}
+				?>
+					<div class="col-lg-4 col-md-6 col-sm-6 col-6">
+					<aside id="nav_menu-3" class="widget widget_text sml-device widget_news">
+		        <h3 class="widget-title">News</h3>
+		        <div class="menu-menu-1-container">
+		          <ul id="menu-menu-3" class="menu">
+		          <?php $news = get_posts(
+		          array(
+		            'post_type'=> 'post',
+		            'orderby'    => 'ID',
+		            'post_status' => 'publish',
+		            'order'    => 'DESC',
+								'category_name' => 'news'
+		          )
+		        );
+		        foreach ($news as $singleNews) {?>
+		          <li class="menu-item menu-item-type-post_type menu-item-object-page">
+		          <a href="<?php echo get_permalink( $singleNews -> ID ); ?>"><?php echo get_the_title( $singleNews -> ID ); ?></a>
+		        </li>
+					<?php } ?>
+						  </ul>
+					  </div>
+					</aside>
+				</div>
+				<?php }?>
 
 
 </div>
